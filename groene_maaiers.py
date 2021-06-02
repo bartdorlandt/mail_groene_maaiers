8aa1c7fc308cec556f4336f128b9526195a47336
Bart <bart@bamweb.nl>
2021-06-02 22:10:35 +0200

    flake8 modified to include venv. Rewritten sheets portion to use oauth2 api as well, just like people.

--- groene_maaiers.py
+++ groene_maaiers.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
+import os
 
 # google sheet
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
 
 # google contacts
 from googleapiclient.discovery import build
@@ -14,77 +15,116 @@ from google.oauth2.credentials import Credentials
 # other
 from rich import print
 from datetime import timedelta, date
-# from email.utils import parseaddr
 import re
 from decouple import config
 import smtplib
 import ssl
 from email.message import EmailMessage
-
-# def date_plus_days(days=4):
-#     return date.today() + timedelta(days=days)
+# from email.utils import parseaddr
 
 
-def get_contact_connection(scope):
-    credentials = Credentials.from_authorized_user_file('token.json', scopes=scope)
-    return build('people', 'v1', credentials=credentials)
+def credentials_check_setup(scopes, credentials_file='credentials.json', token_file='token.json'):
+    credentials = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token_file):
+        credentials = Credentials.from_authorized_user_file(token_file, scopes)
+    # If there are no (valid) credentials available, let the user log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credentials_file, scopes)
+            credentials = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token_file, 'w') as token:
+            token.write(credentials.to_json())
 
 
-def get_contact_name_email(service):
-    name_mail_dict = {}
+def set_credentials_for_api(scope, file='token.json'):
+    return Credentials.from_authorized_user_file(file, scopes=scope)
+
+
+def get_contact_name_email(credentials):
+    service = build('people', 'v1', credentials=credentials)
     results = service.people().connections().list(
         resourceName='people/me',
         pageSize=100,
-        personFields='names,emailAddresses').execute()
+        personFields='names,emailAddresses,phoneNumbers,biographies').execute()
+    # personFields='names,emailAddresses').execute()
     connections = results.get('connections', [])
-    for person in connections:
-        name = email = None
+    return extract_contacts_info(connections)
+
+
+def extract_contacts_info(contacts):
+    name_mail_dict = {}
+
+    for person in contacts:
+        name = email = biography = phone = None
         names = person.get('names')
         emails = person.get('emailAddresses')
+        biographies = person.get('biographies')
+        phones = person.get('phoneNumbers')
         if names:
             name = names[0].get('displayName', '')
         if emails:
             email = emails[0].get('value', '')
+        if biographies:
+            biography = biographies[0].get('value', '')
+        if phones:
+            phone = phones[0].get('value', '')
 
-        if name and email:
-            name_mail_dict[name] = email
+        name_mail_dict[name] = {
+            'name': name,
+            'email': email,
+            'notes': biography,
+            'phone': phone,
+        }
     return name_mail_dict
 
 
-def get_sheet_connection(scope):
-    # use credentials to create a client to interact with the Google Drive API
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        'credentials.json', scope)
-    client = gspread.authorize(credentials)
-    # use the name of the sheet
-    return client.open("binnentuin_schema").sheet1
+def get_sheet(credentials):
+    sheet_id = '1VVlJBiXnuQ_kw56RgumIyCln3yQks5RyjMGLwy3nttE'
+    sheet_range = 'Blad1!1:26'
+    service = build('sheets', 'v4', credentials=credentials)
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=sheet_id,
+                                range=sheet_range).execute()
+    return result.get('values', [])
 
 
-# def get_datetime_of_string(string):
-#     """input = 12-06"""
-#     year = date.today().year
-#     day, month = string.split('-')
-#     return date(year, int(month), int(day))
+def get_sheet_row(sheet_list, short_date):
+    for line in sheet_list:
+        if line[0] == short_date:
+            return line
+
+
+def get_sheet_row_names(row, index=5):
+    try:
+        return row[index]
+    except IndexError:
+        # todo, email bart
+        print('Names not found. row: %s' % row)
 
 
 def get_next_saturday_datetime():
     today = date.today()
-    saturday = today + timedelta((5-today.weekday()) % 7)
+    saturday = today + timedelta((5 - today.weekday()) % 7)
     return saturday.strftime("%d-%m")
 
 
-def get_index_number_of_datum_col(sheet):
-    """Adding one, because of the header"""
-    cols = sheet.col_values(1)
-    return cols.index(get_next_saturday_datetime()) + 1
-
-
-def get_names_cell(sheet, index):
-    return sheet.cell(index, 6).value
+# def get_index_number_of_datum_col(sheet):
+#     """Adding one, because of the header"""
+#     cols = sheet.col_values(1)
+#     return cols.index(get_next_saturday_datetime()) + 1
+# def get_names_cell(sheet, index):
+#     return sheet.cell(index, 6).value
 
 
 def find_email_based_on_name_list(name, contact_dict):
-    return [contact_dict[key] for key in contact_dict.keys()
+    return [contact_dict[key]['email'] for key in contact_dict.keys()
             if key.lower().startswith(name.strip().lower())]
 
 # def find_emails():
@@ -95,11 +135,22 @@ def find_email_based_on_name_list(name, contact_dict):
 #     return set(match)
 
 
+def gen_email_list(names_list, contacts):
+    mailing_list = []
+    for name in names_list:
+        emails = find_email_based_on_name_list(name, contacts)
+        if len(emails) != 1:
+            print('Action Bart. Name: %r result is a non-unique entry. Found: %s' % (name, emails))
+        else:
+            mailing_list.extend(emails)
+    return mailing_list
+
+
 def make_mail_message(mail_from, mail_to):
     msg = EmailMessage()
     subject = "Groen onderhoud reminder %s" % get_next_saturday_datetime()
     # TODO make a nice body text
-    body = 'test voor bart'
+    body = 'test for Bart'
     msg.set_content(body)
 
     msg['Subject'] = subject
@@ -126,34 +177,22 @@ def send_email(receiver):
 
 
 def main():
-    scope_sheet = (
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive",
-        # "https://spreadsheets.google.com/feeds",
-        # "https://www.googleapis.com/auth/drive.file",
-    )
-    scope_contacts = ['https://www.googleapis.com/auth/contacts.readonly']
+    scopes = [
+        "https://www.googleapis.com/auth/contacts.readonly",
+        "https://www.googleapis.com/auth/spreadsheets.readonly"
+    ]
+    credentials_check_setup(scopes)
+    credentials = set_credentials_for_api(scopes)
+    contacts = get_contact_name_email(credentials)
 
-    sheet = get_sheet_connection(scope_sheet)
-    contact_service = get_contact_connection(scope_contacts)
-    contacts = get_contact_name_email(contact_service)
+    sheet_list = get_sheet(credentials)
+    sheet_row = get_sheet_row(sheet_list=sheet_list, short_date=get_next_saturday_datetime())
+    names = get_sheet_row_names(row=sheet_row)
+    names_list = names.split(',')
 
-    # saturday = get_next_saturday_datetime()
-    index = get_index_number_of_datum_col(sheet)
-    who = get_names_cell(sheet, index)
-    who_list = who.split(',')
-
-    mailing_list = []
-    for name in who_list:
-        emails = find_email_based_on_name_list(name, contacts)
-        if len(emails) != 1:
-            print('Action Bart. Name: %r result is non-unique entry'
-                  % name)
-        else:
-            mailing_list.extend(emails)
-
+    mailing_list = gen_email_list(names_list=names_list, contacts=contacts)
     # send email to list of people
-    print(mailing_list)
+    print(set(mailing_list))
 
 
 if __name__ == '__main__':
