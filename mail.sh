2bf73c6fb28e0e43f7f51421a1955f34c859c19e
Bart Dorlandt <bart@bamweb.nl>
2025-06-11 12:33:51 +0200

    Updated mail body

--- mail.sh
+++ mail.sh
@@ -5,8 +5,8 @@ NAME=mail_groene_maaiers
 VERSION=latest
 
 docker_image=${DOCKER_USER}/${NAME}
-docker pull "${docker_image}:${VERSION}"
+# docker pull "${docker_image}:${VERSION}"
 docker run --rm \
     -v $(pwd)/.env:/app/.env \
     -v $(pwd)/credentials.json:/app/credentials.json \
-    "${docker_image}":"${VERSION}" main.py
+    "${docker_image}:${VERSION}" main.py
